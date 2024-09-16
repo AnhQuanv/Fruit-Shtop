fd91b5a39307d359b200efa22d4ad2db3e74646f
banhkute200 <anhquanvo911@gmail.com>
2024-09-16 17:48:06 +0700

    Design Header P2

--- fe/fruitshop/src/pages/user/Theme/Header/Header.js
+++ fe/fruitshop/src/pages/user/Theme/Header/Header.js
@@ -1,8 +1,48 @@
 import { Link } from 'react-router-dom';
 import './header.scss'
-import { AiOutlineFacebook, AiOutlineGlobal, AiOutlineInstagram, AiOutlineLinkedin, AiOutlineMail, AiOutlineUser } from 'react-icons/ai'
+import { AiOutlineFacebook, AiOutlineGlobal, AiOutlineInstagram, AiOutlineLinkedin, AiOutlineMail, AiOutlineShoppingCart, AiOutlineUser } from 'react-icons/ai'
 import { Formater } from '../../../../utils/Formater';
+import { useState } from 'react';
+import { ROUTERS } from 'utils/Router';
 const Header = () => {
+
+    const [menu, setMenu] = useState([
+        {
+            name: 'Home',
+            path: ROUTERS.USER.HOME,
+        },
+        {
+            name: 'STORE',
+            path: ROUTERS.USER.PRODUCTS,
+        },
+        {
+            name: 'PRODUCT',
+            path: ROUTERS.USER.HOME,
+            isShowSubMenu: false,
+            child: [
+                {
+                    name: 'Meat',
+                    path: ""
+                },
+                {
+                    name: 'Meat',
+                    path: ""
+                },
+                {
+                    name: 'Meat',
+                    path: ""
+                },
+            ]
+        },
+        {
+            name: 'BLOG',
+            path: ROUTERS.USER.HOME,
+        },
+        {
+            name: 'CONTACT',
+            path: ROUTERS.USER.HOME,
+        },
+    ]);
     return (
         <>
             <div className="header-top">
@@ -49,7 +89,57 @@ const Header = () => {
                     </div>
                 </div>
             </div>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-xl-3'>
+                        <div className='header-logo'>
+                            <h1>FruitShop</h1>
+                        </div>
+                    </div>
+                    <div className='col-xl-6'>
+                        <nav className='header-menu'>
+                            <ul>
+                                {menu?.map((item, index) => (
+                                    <li key={index} className={index === 0 ? "active" : ""} >
+                                        <Link to={item?.path}>{item.name}</Link>
+                                        {
+                                            item.child && (
+                                                <ul className='header-menu-dropdown'>
+                                                    {
+                                                        item.child.map((childItem, indexChild) => (
+                                                            <li key={`${index}-${indexChild}`}>
+                                                                <Link to={childItem.path}>
+                                                                    {childItem.name}
+                                                                </Link>
+                                                            </li>
 
+                                                        ))
+                                                    }
+                                                </ul>
+                                            )
+                                        }
+                                    </li>
+                                ))}
+                            </ul>
+                        </nav>
+                    </div>
+                    <div className='col-xl-3'>
+                        <div className='header-cart'>
+                            <div className='header-cart-price'>
+                                <span>{Formater(101123)} </span>
+                            </div>
+                            <ul>
+                                <li>
+                                    <Link to='#'>
+                                        <AiOutlineShoppingCart />
+                                        <span>5</span>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div >
         </>
     )
 }
